abd361d04b44686e8dcd5e2e9f0799572e60d8cb
ilya <illyakobylatskyi@gmail.com>
2020-10-26 22:14:09 +0200

    code refactoring

---
+++ config/config.py
@@ -0,0 +1,4 @@
+class EcommerceConfig :
+    SECRET_KEY = 'hfugorhfjfun40r95004urfgmcn92umxgorr4657uytrrevc'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///ecommerce.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
